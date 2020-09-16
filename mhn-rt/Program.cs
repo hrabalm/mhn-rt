2b1b8287f6219fc9e4dbab3793926d54de2c28fe
Miroslav Hrabal <mhn@email.cz>
2020-09-16 13:53:16 +0200

    Remove old code

--- mhn-rt/Program.cs
+++ mhn-rt/Program.cs
@@ -101,79 +101,29 @@ namespace mhn_rt
 
         static void Main(string[] args)
         {
-            // test
-            //var m = Matrix4d.CreateTranslation(0, 0, 5);
-            //var v = Vector3d.Transform(new Vector3d(0, 0, 0), m);
             SceneNode root = new SceneNode();
             Scene scene = new Scene()
             {
                 RootIntersectable = root,
-                //Camera = new Camera(),
                 BackgroundColor = new Vector3(0xA9 / 255.0f, 0xA9 / 255.0f, 0xA9 / 255.0f),
-                //BackgroundColor = new Vector3(230 / 255.0f, 0 / 255.0f, 0 / 255.0f),
             };
-            
 
-            //IMaterial m1 = new LambertianMaterial(new Vector3(0.8f, 0.8f, 0.0f));
-            //IMaterial m2 = new MetalMaterial(new Vector3(0.8f, 0.8f, 0.8f));
+            scene = TestScenes.TestScene1();
 
+            int width = 1280;
+            int height = 720;
+            int sqrtSpp = 1;
+            string filename = "test2.png";
 
-            //root.objects.Add(new Sphere(new Vector3d(0, -2, 0), 0.5, m1));
-            //root.objects.Add(new Sphere(new Vector3d(0, -101.5, -1), 100, m2));
-            //root.objects.Add(new Sphere(new Vector3d(-1.2, 0, -1.5), 0.5, m2));
-
-
-            //var TM = new TriangleManager();
-            //TM.AddTriangle(new Vector3(0, 0, -1.0f), new Vector3(1.0f, 1.0f, -1.0f), new Vector3(0, 1.0f, -1.0f));
-            //TM.BuildBVH();
-            //root.objects.Add(TM);
-
-            //root.AddChild(TestScenes.Oak());
-            //root.AddChild(TestScenes.Bunny());
-            //root.AddChild(TestScenes.Sylvanas());
-            //root.AddChild(TestScenes.Square());
-            root.AddChild(TestScenes.TestScene1());
-            //root.AddChild(TestScenes.Ciri());
-            //root.AddChild(TestScenes.Cat());
-            //root.AddChild(TestScenes.Teapot());
-            //root.AddChild(TestScenes.Dragon());
-            //root.AddChild(TestScenes.Buddha());
-            //root.AddChild(TestScenes.Erato());
-
-            //root.objects.Add(new Sphere(new Vector3d(-2.5, 0, -9.75), 0.45, m1));
-            //root.objects.Add(new Sphere(new Vector3d(-8, 0, -12.75), 3, m1));
-            //root.objects.Add(new Sphere(new Vector3d(-0, 0, -75), 60, m1));
-
-            //ITexture test_texture = new BitmapTexture("t_01__cat_cs_d01.png"); // TODO:Remove
-            //ITexture test_texture = new BitmapTexture("ab.jpg"); // TODO:Remove
-            ITexture test_texture = new BitmapTexture("Sylvanas_ladysylvanaswindrunner_01.png"); // TODO:Remove
-            root.Material = new PhongMaterial();
-            (root.Material as PhongMaterial).Texture = test_texture;
-            (root.Material as PhongMaterial).Color = new Vector3(1, 1, 1);
-            (root.Material as PhongMaterial).Kd = 0.75;
-            (root.Material as PhongMaterial).Ka = 0.25;
-            (root.Material as PhongMaterial).Ks = 0.00;
-
-            Camera camera = new Camera(new Vector3d(0.0, 0.0, 0.5), new Vector3d(0.0, 0.0, -1.0), new Vector3d(0.0, 1.0, 0.0));
-            scene.Camera = camera;
-            //scene.LightSources.Add(new PointLight { Position = new Vector3d(-3.0, 0, -7), Intensity=20});
-            scene.LightSources.Add(new DirectionalLight { Direction = new Vector3d(0.0, -0.5, -1.0).Normalized(), Intensity=1.0});
-
-            //IRayTracer raytracer = new NormalRayTracer();
             IRayTracer raytracer = new SimpleRayTracer();
+            //IRayTracer raytracer = new NormalRayTracer();
             var renderer = new Renderer(raytracer);
-            var output = renderer.Render(scene, 1280, 720, 3);
-            //var output = renderer.Render(scene, 1920, 1080, 2);
-            //var output = renderer.Render(scene, 2*1920, 2*1080, 15);
+            var output = renderer.Render(scene, width, height, sqrtSpp);
 
+            output.Save(filename);
+            Process.Start(filename);
 
-            output.Save("test2.png");
-            Process.Start("test2.png");
-
-            Console.Write(Statistics.Print());
-
-            //Console.WriteLine(stopwatch.Elapsed.TotalSeconds + "s");
-            
+            Console.Write(Statistics.Print()); 
         }
     }
 }
