27cffaecb10253fd5a76dc68115e228daedd4642
Miroslav Hrabal <mhn@email.cz>
2020-09-18 18:29:22 +0200

    Remove the need to lock texture

--- mhn-rt/Texture.cs
+++ mhn-rt/Texture.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
 
 namespace mhn_rt
 {
@@ -58,15 +59,44 @@ namespace mhn_rt
 
     class BitmapTexture : ITexture
     {
-        Bitmap img;
+        byte[] img2;
+
+        int Width;
+        int Height;
 
         public BitmapTexture(string filename)
         {
-            img = new Bitmap(filename);
+            // Convert original texture to ARGB
+            Bitmap img = new Bitmap(filename);
+            Bitmap temp = new Bitmap(img.Width, img.Height, PixelFormat.Format32bppArgb);
+            
+            using (Graphics g = Graphics.FromImage(temp))
+            {
+                g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height));
+            }
+
+            // copy image data into an array (Bitmap.GetPixel doesn't support simultaneous access from multiple threads)
+            var data = temp.LockBits(new Rectangle(0, 0, temp.Width, temp.Height), ImageLockMode.ReadOnly, temp.PixelFormat);
+            img2 = new byte[temp.Width * temp.Height * 4];
+            Marshal.Copy(data.Scan0, img2, 0, img2.Length);
+            temp.UnlockBits(data);
+            Width = temp.Width;
+            Height = temp.Height;
         }
         public Vector3d GetColor(Vector2 uv, Vector3d point, out double alpha)
         {
             return GetColorInterp(uv, out alpha);
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            int offset = (y * Width + x) * 4;
+            byte r = img2[offset + 2];
+            byte g = img2[offset + 1];
+            byte b = img2[offset + 0];
+            byte a = img2[offset + 3];
+
+            return Color.FromArgb(a, r, g, b);
         }
 
         public Vector3d GetColorInterp(Vector2 uv, out double alpha)
@@ -94,50 +124,46 @@ namespace mhn_rt
             while (y > 1.0)
                 y -= 1.0;
 
-            lock (img) // TODO: Direct memory access instead of GetPixel?
+            x = (Width - 1) * x;
+            y = (Height - 1) * y;
+
+            int x1, x2, y1, y2;
+            x1 = (int)Math.Floor(x);
+            x2 = (int)Math.Ceiling(x);
+            y1 = (int)Math.Floor(y);
+            y2 = (int)Math.Ceiling(y);
+
+            double dx = x - x1;
+            double dy = y - y1;
+
+
+            if (bilinear && x1 >= 0 && x2 < Width && y1 >= 0 && y2 < Height) // enough pixels for bilinear interp.
             {
-
-                x = (img.Width - 1) * x;
-                y = (img.Height - 1) * y;
-
-                int x1, x2, y1, y2;
-                x1 = (int)Math.Floor(x);
-                x2 = (int)Math.Ceiling(x);
-                y1 = (int)Math.Floor(y);
-                y2 = (int)Math.Ceiling(y);
-
-                double dx = x - x1;
-                double dy = y - y1;
+                var c11 = GetPixel(x1, y1);
+                var c12 = GetPixel(x1, y2);
+                var c21 = GetPixel(x2, y1);
+                var c22 = GetPixel(x2, y2);
 
 
-                if (bilinear && x1 >= 0 && x2 < img.Width && y1 >= 0 && y2 < img.Height) // enough pixels for bilinear interp.
-                {
-                    var c11 = img.GetPixel(x1, y1);
-                    var c12 = img.GetPixel(x1, y2);
-                    var c21 = img.GetPixel(x2, y1);
-                    var c22 = img.GetPixel(x2, y2);
+                //if (c11.A != 255)
+                //    Co
 
-
-                    //if (c11.A != 255)
-                    //    Co
-
-                    R = ((1 - dx) * (1 - dy) * c11.R + (1 - dx) * dy * c12.R + dx * (1 - dy) * c21.R + dx * dy * c22.R) / 255.0;
-                    G = ((1 - dx) * (1 - dy) * c11.G + (1 - dx) * dy * c12.G + dx * (1 - dy) * c21.G + dx * dy * c22.G) / 255.0;
-                    B = ((1 - dx) * (1 - dy) * c11.B + (1 - dx) * dy * c12.B + dx * (1 - dy) * c21.B + dx * dy * c22.B) / 255.0;
-                    A = ((1 - dx) * (1 - dy) * c11.A + (1 - dx) * dy * c12.A + dx * (1 - dy) * c21.A + dx * dy * c22.A) / 255.0;
-                }
-                else // otherwise we just select nearest neighbour
-                {
-                    var t = img.GetPixel((int)Math.Round(x), (int)Math.Round(y));
-
-                    R = t.R / 255.0;
-                    G = t.G / 255.0;
-                    B = t.B / 255.0;
-                    A = t.A / 255.0;
-                }
-
-                alpha = A;
+                R = ((1 - dx) * (1 - dy) * c11.R + (1 - dx) * dy * c12.R + dx * (1 - dy) * c21.R + dx * dy * c22.R) / 255.0;
+                G = ((1 - dx) * (1 - dy) * c11.G + (1 - dx) * dy * c12.G + dx * (1 - dy) * c21.G + dx * dy * c22.G) / 255.0;
+                B = ((1 - dx) * (1 - dy) * c11.B + (1 - dx) * dy * c12.B + dx * (1 - dy) * c21.B + dx * dy * c22.B) / 255.0;
+                A = ((1 - dx) * (1 - dy) * c11.A + (1 - dx) * dy * c12.A + dx * (1 - dy) * c21.A + dx * dy * c22.A) / 255.0;
             }
+            else // otherwise we just select nearest neighbour
+            {
+                var t = GetPixel((int)Math.Round(x), (int)Math.Round(y));
+
+                R = t.R / 255.0;
+                G = t.G / 255.0;
+                B = t.B / 255.0;
+                A = t.A / 255.0;
+            }
+
+            alpha = A;
 
             return new Vector3d(R, G, B);
         }
