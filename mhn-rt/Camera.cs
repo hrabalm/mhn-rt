bead0691f13167b25d097c7c621e779a12f55662
Miroslav Hrabal <mhn@email.cz>
2020-09-21 18:26:47 +0200

    Make Camera a bit more flexible

--- mhn-rt/Camera.cs
+++ mhn-rt/Camera.cs
@@ -1,10 +1,5 @@
 ﻿using OpenTK;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace mhn_rt
 {
@@ -13,29 +8,48 @@ namespace mhn_rt
         /// <summary>
         /// vertical FOV angle, has to be < 180.0 
         /// </summary>
-        double verticalFOV = (90.0/360.0)*MathHelper.TwoPi;
-        double aspectRatio = 16.0 / 9.0;
-        double viewportHeight = 2.0;
+        double verticalFOV;
+        double aspectRatio;
+        double viewportHeight;
         double viewportWidth;
-        double focalLength = 1.0;
+        double focalLength;
 
+        // these are in world coords
         Vector3d origin = Vector3d.Zero;
         Vector3d horizontal;
         Vector3d vertical;
         Vector3d bottomLeftCorner;
 
-        //Vector3d lookFrom;
-        //Vector3d lookAt;
-        //Vector3d up;
+        Vector3d lookFrom;
+        Vector3d lookAt;
+        Vector3d up;
 
-        public Camera(Vector3d lookFrom, Vector3d lookAt, Vector3d up)
+        public Camera(Vector3d lookFrom, Vector3d lookAt, Vector3d up) : this(lookFrom, lookAt, up, 16.0 / 9.0, 90) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lookFrom"></param>
+        /// <param name="lookAt"></param>
+        /// <param name="up"></param>
+        /// <param name="aspectRatio"></param>
+        /// <param name="verticalFOV">Vertical FOV in degrees</param>
+        public Camera(Vector3d lookFrom, Vector3d lookAt, Vector3d up, double aspectRatio, double verticalFOV)
         {
-            //viewportWidth = aspectRatio * viewportHeight;
-            //horizontal = new Vector3d(viewportWidth, 0, 0);
-            //vertical = new Vector3d(0, viewportHeight, 0);
-            //bottomLeftCorner = origin - (horizontal / 2)
-            //    - (vertical / 2) - new Vector3d(0, 0, focalLength);
+            this.aspectRatio = aspectRatio;
+            this.verticalFOV = (verticalFOV / 360.0) * MathHelper.TwoPi;
+            viewportHeight = 2.0;
+            focalLength = 1.0;
 
+            this.lookAt = lookAt;
+            this.lookFrom = lookFrom;
+            this.up = up;
+
+            Recalculate();
+        }
+
+        void Recalculate()
+        {
             var h = Math.Tan(verticalFOV / 2);
             viewportHeight = 2.0 * h;
             viewportWidth = aspectRatio * viewportHeight;
@@ -55,7 +69,7 @@ namespace mhn_rt
         }
 
         /// <summary>
-        /// Get ray coming from origin going through viewport coords u, v
+        /// Get ray coming from origin going through viewport coords u, v (both [0.0, 1.0])
         /// </summary>
         /// <param name="u"></param>
         /// <param name="v"></param>
